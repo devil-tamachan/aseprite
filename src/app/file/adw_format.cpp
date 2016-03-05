8ef766c2b2e7079d47635b8e7aaea5e39290075a
devil.tamachan <devil.tamachan@gmail.com>
2016-03-06 01:03:34 +0900

    remove wrong name https://github.com/devil-tamachan/aseprite/commit/51ea21dcf1a2593a62da74732e3f0fe339428e62#commitcomment-16518012

--- src/app/file/adw_format.cpp
+++ src/app/file/adw_format.cpp
@@ -1,5 +1,3 @@
-// Aseprite
-// Copyright (C) 2001-2015  David Capello
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
